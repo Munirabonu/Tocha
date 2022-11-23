839651b1a5c444b9a9fde432b6f3103f2d4c539a
Munirabonu <muniratillaxojayeva@gmail.com>
2022-11-23 09:56:13 +0500

    page_file

---
+++ src/component/Generic/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Generic() {
+  return (
+    <div>
+      Generic
+    </div>
+  )
+}
---
+++ src/page/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Generic from '../component/Generic/Index'
+
+export default function Home() {
+  return (
+    <div>
+      <Generic />
+    </div>
+  )
+}
