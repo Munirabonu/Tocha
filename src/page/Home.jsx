35306859bfeb752f6ddf3f9c5c8463f2a7f48c8e
Soliha <solyatursuntasheva@gmail.com>
2022-11-23 12:27:49 +0500

    Made Tocha Online section
    Let's continue

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Generic from '../component/Generic/Index'
-
+import TochaOnline from "../component/tochaOnline/Index"
+import "../scss/styles"
 export default function Home() {
   return (
     <div>
       <Generic />
+      <TochaOnline/>
     </div>
   )
 }
