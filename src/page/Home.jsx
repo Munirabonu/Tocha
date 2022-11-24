36bb4f4f1f14e9eb914558f29b169d1316561c54
Soliha <solyatursuntasheva@gmail.com>
2022-11-24 14:19:52 +0500

    Made footer and join chat sections

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Generic from "../component/Generic/Index";
 import TochaOnline from "../component/TochaOnline/Index";
 import Tariffs from "../component/Tariffs/Index";
-import Chat from "../component/JoinChat/Index"
+import Chat from "../component/JoinChat/Index";
+import Footer from "../component/Footer/Index";
 import "../scss/styles";
 export default function Home() {
   return (
     <div>
-      <Chat/>
       <Generic />
       <TochaOnline />
       <Tariffs />
+      <Chat />
+      <Footer />
     </div>
   );
 }
