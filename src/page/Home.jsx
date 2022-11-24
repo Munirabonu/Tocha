e8bbd5934c253dde4354f8adfca2eea7229d70d4
Soliha <solyatursuntasheva@gmail.com>
2022-11-24 11:24:28 +0500

    Made Join our telegram chat section

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Generic from "../component/Generic/Index";
 import TochaOnline from "../component/TochaOnline/Index";
 import Tariffs from "../component/Tariffs/Index";
-
+import Chat from "../component/JoinChat/Index"
 import "../scss/styles";
 export default function Home() {
   return (
     <div>
-      <Generic />
+      <Chat/>
+      {/* <Generic />
       <TochaOnline />
-      <Tariffs />
+      <Tariffs /> */}
     </div>
   );
 }
