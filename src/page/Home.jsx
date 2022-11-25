97bba562349e3acf644ba8098bb34c6f5134baf1
Soliha <solyatursuntasheva@gmail.com>
2022-11-25 12:46:17 +0500

    Start Telegram chat section

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,15 +6,17 @@ import Chat from "../component/JoinChat/Index";
 import Footer from "../component/Footer/Index";
 import "../scss/styles";
 import Feedback from "../component/Feedback/Index";
+import TelegramChat from "../component/TelegramChat/Index"
 export default function Home() {
   return (
     <div>
+      <TelegramChat/>
       {/* <Generic />
       <TochaOnline />
       <Tariffs />
+      <Feedback />
       <Chat />
       <Footer /> */}
-      <Feedback />
     </div>
   );
 }
