d5cd173f7265ae8bfe06b1cbb2954d186983b07a
Munirabonu <muniratillaxojayeva@gmail.com>
2022-12-02 00:37:31 +0500

    header

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,6 @@
-
-import React from 'react'
-import Generic from '../component/Generic/Index'
-import Header from '../component/Header/Index'
-import Navbar from '../component/Navbar/Index'
-
+import React from "react";
+import Header from "../component/Header/Index";
+import Navbar from "../component/Navbar/Index";
 
 export default function Home() {
   return (
