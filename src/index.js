6ffc55b408296a23b952fee3adac5eb3fd2c3f9b
Soliha <solyatursuntasheva@gmail.com>
2022-12-02 20:03:06 +0500

    gooo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./page/Home";
-
+// tets
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
