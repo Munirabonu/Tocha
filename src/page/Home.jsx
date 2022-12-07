04671aebfbed975092594db76cd86871f6bed843
Munirabonu <muniratillaxojayeva@gmail.com>
2022-12-07 20:54:43 +0500

    test

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../component/Header/Index";
 import Navbar from "../component/Navbar/Index";
-import TochaOnline from "../component/TochaOnline/Index";
+// import TochaOnline from "../component/TochaOnline/Index";
 import Tariffs from "../component/Tariffs/Index";
 import Chat from "../component/JoinChat/Index";
 import Footer from "../component/Footer/Index";
@@ -15,7 +15,7 @@ export default function Home() {
     <div>
       <Navbar />
       <Header />
-      <TochaOnline />
+      {/* <TochaOnline /> */}
       <Tariffs />
       <TelegramChat />
       <Feedback />
