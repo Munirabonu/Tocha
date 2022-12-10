692125b2f842dd64ee71d14222cc7b4beb08eeee
Munirabonu <muniratillaxojayeva@gmail.com>
2022-12-10 15:51:33 +0500

    done

---
+++ src/component/Loading/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.scss";
+export default function Loading() {
+  return (
+    <div className="loader_wrap">
+      <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import SignIn from "../component/SignIn/Index";
-import SignUp from "../component/SignUp/Index";
-import Home from "../page/Home";
-// const Home=React.lazy(()=>import('../page/Home'))
+import Loading from "../component/Loading/Index";
+const Home=React.lazy(()=>import("../page/Home"))
+const SignUp=React.lazy(()=>import("../component/SignUp/Index"))
+const SignIn=React.lazy(()=>import("../component/SignIn/Index"))
 
 export const Root = () => {
   return (
-    <Routes> 
-       <Route path="/" element={<Home />} />
-       <Route path="/home" element={<Home />} />
-       <Route path="/signup" element={<SignUp />} />
-       <Route path="/signin" element={<SignIn />} />
-      <Route path="*" element={<div>not found</div>} />
-    </Routes>
+    <React.Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
+    </React.Suspense>
   );
 };
-
-
